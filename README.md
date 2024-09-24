95d966dc54fb82d215f0710148cfad579a818a86
D. Sunshine <david@sunshines.org>
2024-09-24 03:28:59 +0000

    Documentation updates

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 
 # Installing
 
+This will update nix flakes and then perofrm a `nixos-rebuild switch`.
 ```bash
-sudo nixos-rebuild switch --flake . --impure
+sudo make rebuild
 ```
--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # nixvim flake
     nixvim.url = "github:dsunshi/nixvim";
   };
   outputs = { self, nixpkgs, nixvim, ... }@inputs:
@@ -9,6 +10,7 @@
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs outputs; };
+        # Remainder of the NixOS configuration
         modules = [ ./configuration.nix ];
       };
     };
