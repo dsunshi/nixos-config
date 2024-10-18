aef80b7e65bd05dca7062cc0596ad6c5a8857386
D. Sunshine <david@sunshines.org>
2024-10-17 20:45:09 -0400

    Ease at the cost of security ;)

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,5 @@
 #! nix-shell -i bash
 git clone https://github.com/dsunshi/nixos-config.git
 cd nixos-config
+direnv allow # DANGEROUS! You should **not** allow this
 sudo nixos-rebuild switch --flake .
