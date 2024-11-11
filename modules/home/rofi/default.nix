cdf969e5dda1eeecb10b32e18ac0e176a147e44b
D. Sunshine <david@sunshines.org>
2024-11-11 10:20:37 -0500

    Rofi improvements

--- modules/home/rofi/default.nix
+++ modules/home/rofi/default.nix
@@ -1,7 +1,7 @@
 { myUser, pkgs, ... }: {
   home-manager.users.${myUser.username}.home = {
-    packages = with pkgs; [ rofi-wayland ];
-    file.".local/bin/rofi-power-menu".source = ./../bin/rofi-power-menu;
+    packages = with pkgs; [ rofi-wayland rofi-power-menu rofi-bluetooth ];
+    # file.".local/bin/rofi-power-menu".source = ./../bin/rofi-power-menu;
     file.".config/rofi/config.rasi".source = ./config.rasi;
     file.".config/rofi/colorscheme.rasi".source = ./colorscheme.rasi;
   };
