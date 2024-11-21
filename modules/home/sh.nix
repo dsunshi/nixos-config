8109019d3df62f908ff3f02bbe91468b59c3c228
D. Sunshine <david@sunshines.org>
2024-11-21 14:19:58 -0500

    Exit yazi in the current directory

--- modules/home/sh.nix
+++ modules/home/sh.nix
@@ -36,6 +36,18 @@ in {
           ''
             nix-shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [ $argv ])"
           '';
+        # This y shell wrapper that provides the ability to change the current working directory when exiting Yazi.
+        # Then use y instead of yazi to start, and press q to quit, you'll see the CWD changed.
+        # Sometimes, you don't want to change, press Q to quit.
+        y = # fish
+          ''
+            set tmp (mktemp -t "yazi-cwd.XXXXXX")
+            	yazi $argv --cwd-file="$tmp"
+            	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            		builtin cd -- "$cwd"
+            	end
+            	rm -f -- "$tmp"
+          '';
       };
       plugins = [
         {
