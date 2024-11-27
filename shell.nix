fede7e093c6abd7a97b3aae0a96d1b427a3ee1b1
D. Sunshine <david@sunshines.org>
2024-11-27 14:50:48 +0000

    Special WSL hooks
    WSL does not like to seem to build the latest GHC + XMonad, since these
    are not really needed on WSL they are now excluded.

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
 { nixpkgs ? import <nixpkgs> { }, compiler ? "ghc98" }:
 let
+  isWSL = if builtins.pathExists /etc/wsl.conf then true else false;
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps:
-    with ps; [
+    with ps; (if isWSL then [] else [
       # libraries
       xmonad_0_18_0
       xmonad-utils
@@ -12,15 +13,17 @@ let
       hint
       # "tools"
       haskell-language-server # This must match the GHC version in order to work
-    ]);
+    ]));
 in pkgs.stdenv.mkDerivation {
   name = "NixOS development environment";
   buildInputs = with pkgs; [
-    ghc
     gnumake
-    haskellPackages.hoogle
-    cabal-install # https://github.com/NixOS/nixpkgs/issues/321569
-  ];
+    ] ++ (if !isWSL then [
+      # Only include ghc if we will develop XMonad on actual NixOS 
+      ghc
+      haskellPackages.hoogle
+      cabal-install # https://github.com/NixOS/nixpkgs/issues/321569
+    ] else []);
   shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
   env = { FLAKE = builtins.getEnv "PWD"; };
 }
