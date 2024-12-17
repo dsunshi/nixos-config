37dbe702a87139087d323ea36d335ecd16ed3b66
D. Sunshine <david@sunshines.org>
2024-12-17 12:48:26 -0500

    Working list of keys, remove old sddm.nix

--- modules/home/xmonad/lib/KeyExport.hs
+++ modules/home/xmonad/lib/KeyExport.hs
@@ -2,11 +2,18 @@ module Main (main) where
 
 import Keys
 import XMonad hiding (defaultConfig)
+import XMonad.Prelude
 import XMonad.Util.NamedActions
 
+-- writeFile "keybindings" (unlines $ showKm x)
 showKeybindings' :: [((KeyMask, KeySym), NamedAction)] -> IO ()
 showKeybindings' x = do
-  writeFile "keybindings" (unlines $ showKmSimple x)
+  writeFile "keybindings" (unlines file)
+  where
+    file = map keyP x
+
+keyP :: ((KeyMask, KeySym), NamedAction) -> String
+keyP ((km, ks), action) = if ks == 0 then "" else keyToString (km, ks)
 
 keybindings :: [((KeyMask, KeySym), NamedAction)]
 keybindings = myKeys def
