3d395e2cf64f7ce85ca000ccafcf3bcd8b8d02e4
D. Sunshine <david@sunshines.org>
2025-04-10 15:10:58 -0400

    Switching to offload seems to fix flicker issues

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -14,12 +14,12 @@
         package = config.boot.kernelPackages.nvidiaPackages.stable;
         # package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
         prime = {
-          sync.enable = true;
+          # sync.enable = true;
           # TODO: specialisation?
-          # offload = {
-          #   enable = true;
-          #   enableOffloadCmd = true;
-          # };
+          offload = {
+            enable = true;
+            enableOffloadCmd = true;
+          };
           # FIXME: Change the following values to the correct Bus ID values for your system!
           intelBusId = "PCI:0:2:0";
           nvidiaBusId = "PCI:1:0:0";
